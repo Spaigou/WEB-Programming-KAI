90f1db69a6718b83721ad7ce823f3da1d134a03f
Spaigou <mirzahanov.chingiz@gmail.com>
2022-05-08 18:14:15 +0300

    for cycle realization app.js

--- Amazeriffic/javascript/app.js
+++ Amazeriffic/javascript/app.js
@@ -1,5 +1,11 @@
 var main = function () {
     "use strict";
-    console.log("hello, world!");
+    for (var tabNumber = 1; tabNumber <= 3; tabNumber++) {
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(tabSelector).on("click", function () {
+            $(".tabs span").removeClass("active");
+            $(this).addClass("active");
+            return false;
+        });
+    }
 };
-$(document).ready(main);
